0e49338c824f4df6444b57ccc623a7de3bfdf54c
cndaniel <tigerljt@gmail.com>
2016-12-16 02:54:21 +0800

    downvote

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-   
+
   # For details on the DSL available within this file, see
   root 'topics#index'
   resources:topics do
     member do
       post 'upvote'
+      post 'downvote'
     end
   end
 
