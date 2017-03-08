5d79d5c60d968c8c49a810c29de695fe63191614
cndaniel <cndaniel@me.com>
2017-03-08 04:25:36 +0100

    ajax delete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -60,6 +60,7 @@ class TopicsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
       format.json { head :no_content }
+      format.js
     end
   end
 
