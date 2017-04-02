dddac68be930ad94eb826100f1fd1dcb638158e8
Daniel <i@jiatai.me>
2017-04-02 11:33:46 +0200

    Update topics_controller.rb

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -72,7 +72,7 @@ class TopicsController < ApplicationController
 
   def downvote
     @topic = Topic.find(params[:id])
-    @topic.votes.last.destroy unless topic.votes.empty?
+    @topic.votes.last.destroy unless @topic.votes.empty?
     redirect_to topics_path
   end
   private
